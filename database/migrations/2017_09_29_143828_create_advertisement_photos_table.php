af1d850c37be1c13edb71cfa9e01a72a5de46f85
TaufanFadhilah <tfi_23@ymail.com>
2017-09-29 21:45:06 +0700

    Adding Migration

---
+++ database/migrations/2017_09_29_143828_create_advertisement_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertisementPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisement_photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('advertisementsId')->unsigned();
+            $table->string('path');
+            //$table->timestamps();
+
+            $table->foreign('advertisementsId')->references('id')->on('advertisements')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisement_photos');
+    }
+}
---
+++ database/migrations/2017_09_29_143940_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('clientId')->unsigned();
+            $table->integer('businessId')->unsigned();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('clientId')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('businessId')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
