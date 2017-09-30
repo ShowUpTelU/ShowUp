cf25a03391ac8212130dab3116d14e5393d1240f
TaufanFadhilah <tfi_23@ymail.com>
2017-09-30 09:56:32 +0700

    Update layout

--- resources/views/survey/survey.blade.php
+++ resources/views/survey/survey.blade.php
@@ -5,25 +5,10 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="{{asset('css/materialize.min.css')}}"  media="screen,projection"/>
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
   <title>Up Everything You Want!</title>
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <style media="screen">
-    .nav-logo{
-    max-width: 50px;
-    max-height: 50px;
-    margin: 10px;
-    margin-bottom: 5px;
-    }
-    .parallax-container {
-      min-height: 650px;
-    }
-    .full-height{
-      min-height: 650px;
-      padding: 20px;
-    }
-
-  </style>
 </head>
 <body>
   <!-- START HEADER -->
